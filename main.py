ccc20ed1c580cb950abeac3fef28bd313493386d
DHawk98 <44686772+DHawk98@users.noreply.github.com>
2021-03-15 11:18:36 +0100

    little update

--- main.py
+++ main.py
@@ -1,41 +1,43 @@
-# This is a sample Python script.
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
+import numpy as np
 import cv2
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+from matplotlib import pyplot as plt
 
 
-# Press the green button in the gutter to run the script.
+
+def threshold():
+    img = cv2.imread('orginal.jpg')
+    img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    # img_gray=cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    # cv2.threshold(img, THRESH, MAXVALUE, cv2.METHOD)
+    # ret,img_bin1
+    # cv2.adaptiveThreshold(img, maxvalue, Adaptive Method, BlockSize, C)
+    # th3 = cv2.adaptiveThreshold(img_gray,100,cv2.ADAPTIVE_THRESH_GAUSSIAN_C,cv2.THRESH_BINARY,11,2)
+    # cv2.imshow('bin',th3)
+    # cv2.imwrite('out.tif', th3)
+    # cv2.waitKey(5000)
+    # equ = cv2.equalizeHist(img)
+    # clahe = cv2.createCLAHE(clipLimit=2.0, tileGridSize=(32,32))
+    # cl1 = clahe.apply(img)
+    # res2 = np.hstack((img,cl1))
+    # cv2.imwrite('hist_clahe.tif',cl1)
+    # th2=cv2.adaptiveThreshold(img_gray,160,cv2.ADAPTIVE_THRESH_MEAN_C,cv2.THRESH)
+    # th3 = cv2.adaptiveThreshold(img_gray, 160, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 11,23)
+
+    # img = cv.imread('home.jpg', 0)
+
+    blur = cv2.GaussianBlur(img_gray, (51, 51), 0)
+
+    img_gray_hist = cv2.calcHist([blur], [0], None, [256], [0, 256])
+    th3 = cv2.adaptiveThreshold(img_gray, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 31, 15)
+
+    plt.hist(blur.ravel(), 256, [0, 256]);
+    plt.show()
+
+    cv2.imwrite('test.tif', th3)
+    cv2.waitKey()
+
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    threshold()
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
-
-
-# img_gray=cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-# cv2.threshold(img, THRESH, MAXVALUE, cv2.METHOD)
-# ret,img_bin1
-# cv2.adaptiveThreshold(img, maxvalue, Adaptive Method, BlockSize, C)
-# th3 = cv2.adaptiveThreshold(img_gray,100,cv2.ADAPTIVE_THRESH_GAUSSIAN_C,cv2.THRESH_BINARY,11,2)
-# cv2.imshow('bin',th3)
-# cv2.imwrite('out.tif', th3)
-# cv2.waitKey(5000)
-# equ = cv2.equalizeHist(img)
-# clahe = cv2.createCLAHE(clipLimit=2.0, tileGridSize=(32,32))
-# cl1 = clahe.apply(img)
-# res2 = np.hstack((img,cl1))
-# cv2.imwrite('hist_clahe.tif',cl1)
-# a=[1,2]
-# b=a.copy()
-
-img = cv2.imread('source.tif')
-img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-th2=cv2.adaptiveThreshold(img_gray,160,cv2.ADAPTIVE_THRESH_MEAN_C,cv2.THRESH)
-th3 = cv2.adaptiveThreshold(img_gray, 160, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 11,23)
-cv2.imwrite('test.tif', th3)
-cv2.waitKey()
