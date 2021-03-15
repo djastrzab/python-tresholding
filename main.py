2f80f136d018e92d0d901f7078d440b26aa5f214
DHawk98 <44686772+DHawk98@users.noreply.github.com>
2021-03-15 11:18:53 +0100

    Update main.py

--- main.py
+++ main.py
@@ -9,23 +9,6 @@ from matplotlib import pyplot as plt
 def threshold():
     img = cv2.imread('orginal.jpg')
     img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    # img_gray=cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    # cv2.threshold(img, THRESH, MAXVALUE, cv2.METHOD)
-    # ret,img_bin1
-    # cv2.adaptiveThreshold(img, maxvalue, Adaptive Method, BlockSize, C)
-    # th3 = cv2.adaptiveThreshold(img_gray,100,cv2.ADAPTIVE_THRESH_GAUSSIAN_C,cv2.THRESH_BINARY,11,2)
-    # cv2.imshow('bin',th3)
-    # cv2.imwrite('out.tif', th3)
-    # cv2.waitKey(5000)
-    # equ = cv2.equalizeHist(img)
-    # clahe = cv2.createCLAHE(clipLimit=2.0, tileGridSize=(32,32))
-    # cl1 = clahe.apply(img)
-    # res2 = np.hstack((img,cl1))
-    # cv2.imwrite('hist_clahe.tif',cl1)
-    # th2=cv2.adaptiveThreshold(img_gray,160,cv2.ADAPTIVE_THRESH_MEAN_C,cv2.THRESH)
-    # th3 = cv2.adaptiveThreshold(img_gray, 160, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 11,23)
-
-    # img = cv.imread('home.jpg', 0)
 
     blur = cv2.GaussianBlur(img_gray, (51, 51), 0)
 
